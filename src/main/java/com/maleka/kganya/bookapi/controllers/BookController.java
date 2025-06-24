66bd310900ee808d945cb1a322a63d614e49e66b
kmaleka <kganyamilton@gmail.com>
2025-06-24 12:43:24 +0200

    add: swagger api documentation

--- src/main/java/com/maleka/kganya/bookapi/controllers/BookController.java
+++ src/main/java/com/maleka/kganya/bookapi/controllers/BookController.java
@@ -2,6 +2,9 @@ package com.maleka.kganya.bookapi.controllers;
 
 import com.maleka.kganya.bookapi.entities.Book;
 import com.maleka.kganya.bookapi.services.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,44 +12,62 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
+@Tag(name = "Book Controller", description = "CRUD and borrowing operations for books")
 public class BookController {
 
     @Autowired
     private BookService service;
 
+    @GetMapping("/test")
+    public String test() {
+        return "Working";
+    }
+
+    @Operation(summary = "Get all books")
     @GetMapping
     public List<Book> all() {
         return service.getAllBooks();
     }
 
+    @Operation(summary = "Get a book by ID")
     @GetMapping("/{id}")
-    public Book get(@PathVariable Long id) {
+    public Book get(
+            @Parameter(description = "ID of the book") @PathVariable Long id) {
         return service.getBook(id);
     }
 
+    @Operation(summary = "Create a new book")
     @PostMapping
     public Book create(@RequestBody Book book) {
         return service.create(book);
     }
 
+    @Operation(summary = "Update an existing book")
     @PutMapping("/{id}")
-    public Book update(@PathVariable Long id, @RequestBody Book book) {
+    public Book update(
+            @Parameter(description = "ID of the book to update") @PathVariable Long id,
+            @RequestBody Book book) {
         return service.update(id, book);
     }
 
+    @Operation(summary = "Delete a book")
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public void delete(
+            @Parameter(description = "ID of the book to delete") @PathVariable Long id) {
         service.delete(id);
     }
 
+    @Operation(summary = "Borrow a book")
     @PostMapping("/{id}/borrow")
-    public Book borrow(@PathVariable Long id) {
+    public Book borrow(@Parameter(description = "ID of the book to borrow") @PathVariable Long id) {
         return service.borrowBook(id);
     }
 
+    @Operation(summary = "Return a book")
     @PostMapping("/{id}/return")
-    public Book returnBook(@PathVariable Long id) {
+    public Book returnBook(@Parameter(description = "ID of the book to return") @PathVariable Long id) {
         return service.returnBook(id);
     }
 }
+
 
