dc78b88fbc075b70ea795cf04a527ede0bbc028c
kmaleka <kganyamilton@gmail.com>
2025-06-27 12:20:54 +0200

    test - ssh key sucess

--- src/main/java/com/maleka/kganya/bookapi/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.maleka.kganya.bookapi.exceptions;
-
-public class ResourceNotFoundException extends Exception{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
-
